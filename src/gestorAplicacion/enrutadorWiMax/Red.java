95198a05976265cedbbeaa33c80a8114d7ffcce1
vamoreno23 <152014036+vamoreno23@users.noreply.github.com>
2025-01-19 16:11:59 -0500

    interfaz

--- src/gestorAplicacion/enrutadorWiMax/Red.java
+++ src/gestorAplicacion/enrutadorWiMax/Red.java
@@ -1,1 +1,16 @@
 package gestorAplicacion.enrutadorWiMax;
+
+import java.util.ArrayList;
+import gestorAplicacion.host.Cliente;
+import gestorAplicacion.host.ProveedorInternet;
+
+interface Red{
+  
+  //INTERFACE CREADA PARA QUE LA CLASE SERVIDOR LA IMPLEMENTE
+
+  static final int FLUJO_RED_PRELIMINAR=500;
+
+  ArrayList<Servidor> verificarAdmin(ArrayList<ProveedorInternet> proveedores, String nombre);
+  ArrayList<Integer> distanciasOptimas(ArrayList<Cliente> clientes,ArrayList<Integer> listaIntensidadesClientes);
+
+}
--- src/gestorAplicacion/enrutadorWiMax/Servidor.java
+++ src/gestorAplicacion/enrutadorWiMax/Servidor.java
@@ -3,13 +3,20 @@ package gestorAplicacion.enrutadorWiMax;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Servidor implements Serializable{
+public class Servidor implements Red, Serializable{
+
+  // Mejora tu plan y reporte lo usan 
+
     private String sede;
+    private final double PORCENTAJE_EFICIENCIA;
     private boolean saturado = false;
     private ProveedorInternet proveedor;
     private Plano coordenadas;  
     private static ArrayList<Servidor> servidoresTotales = new ArrayList<>();
     private ArrayList<Router> routers = new ArrayList<>();
+    private final int INDICE_SATURACION; //Numero de clientes que puede tener el servidor 
+    private final int FLUJO_RED_NETO; // Lo que le resta a el servidor 
+
 
     public Servidor(boolean saturado, int INDICE_SATURACION, double pe){
         this.saturado=saturado;
@@ -23,4 +30,32 @@ public class Servidor implements Serializable{
         PORCENTAJE_EFICIENCIA = 0;
         FLUJO_RED_NETO=0;
   }
+
+
+  public static ArrayList<Servidor> buscarServidores(String localidad) {
+    ArrayList<Servidor> serviSede = new ArrayList<Servidor>();
+    for (Servidor servidor : servidoresTotales) {
+      if ((servidor.getSede().equals(localidad)) && (!servidor.isSaturado())) {
+        serviSede.add(servidor);
+      }
+    }
+    return serviSede;
+  }
+  
+  public  ArrayList<Servidor> verificarAdmin(ArrayList<ProveedorInternet> proveedores, String nombre){
+    ArrayList<Servidor> servidoresProveedor = new ArrayList<>();
+    for(ProveedorInternet proveedor: proveedores){
+      if(proveedor.getNombre().equals(nombre)){
+        for(Servidor servidor: servidoresTotales){
+          if(servidor.getProveedor().getNombre().equals(nombre)){
+            servidoresProveedor.add(servidor);
+          }
+        }
+      }
+    }
+    return servidoresProveedor;
+  }
+
+
+
 }
